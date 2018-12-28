89d0e0ba27b8ee1bbefbec54be28e8703b2ad256
Vinit Rai <learner.vinit@gmail.com>
2018-12-28 02:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ return document.querySelector('#nested .target');
 
 function increaseRankBy(n){
 const list = document.querySelectorAll('.ranked-list li');
-(for let i=0;i<list.length;i++){
+for(let i=0;i<list.length;i++){
   list[i].innerHTML = parseInt(list[i].innerHTML)+n;
 }
 }
