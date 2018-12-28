2473b2c4bcd1ef357f8ed740e9e6e9b4a3c1aa59
Vinit Rai <learner.vinit@gmail.com>
2018-12-28 02:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
 
+}
+
+function nestedTarget(){
+return document.querySelector('#nested .target');
+
+}
+
+function increaseRankBy(n){
+const list = document.querySelectorAll('.ranked-list li');
+(for let i=0;i<list.length;i++){
+  list[i].innerHTML = parseInt(list[i].innerHTML)+n;
+}
+}
+
+
+function deepestChild(){
+var lis = document.getElementById('grand-node').querySelectorAll('div');
+var mytest;
+for(var i=0;i<lis.length-1;i++){
+  mytest = lis[i].querySelector('div');
+}
+return mytest;
+
+}
